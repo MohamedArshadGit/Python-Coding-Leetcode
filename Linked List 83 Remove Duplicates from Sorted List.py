fd0d3d5e6ddeee68f2339c3bc6a30436cdac01f1
MohamedArshadGit <123241512+MohamedArshadGit@users.noreply.github.com>
2023-11-15 12:02:12 +0530

    Add files via upload

--- Linked List 83 Remove Duplicates from Sorted List.py
+++ Linked List 83 Remove Duplicates from Sorted List.py
@@ -4,14 +4,14 @@ Medium
 
 Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
 
-Example 1:
+Example SONY Interview Question.py:
 
-Input: head = [1,2,3,3,4,4,5]
-Output: [1,2,5]
+Input: head = [SONY Interview Question.py,2,3,3,4,4,5]
+Output: [SONY Interview Question.py,2,5]
 
 Example 2:
 
-Input: head = [1,1,1,2,3]
+Input: head = [SONY Interview Question.py,SONY Interview Question.py,SONY Interview Question.py,2,3]
 Output: [2,3]
 
 
@@ -44,12 +44,12 @@ def printLinkedList(head):
     print(result)
 
 # Test cases
-# Example 1
+# Example SONY Interview Question.py
 head1 = ListNode(1, ListNode(1, ListNode(2)))
-print("Input Linked List 1:")
+print("Input Linked List SONY Interview Question.py:")
 printLinkedList(head1)
 modified_head1 = deleteDuplicates(head1)
-print("Output Linked List 1:")
+print("Output Linked List SONY Interview Question.py:")
 printLinkedList(modified_head1)
 
 # Example 2
--- Linked List-61 Rotate List-Medium.py
+++ Linked List-61 Rotate List-Medium.py
@@ -3,15 +3,15 @@ Medium
 
 Given the head of a linked list, rotate the list to the right by k places.
 
-Example 1:
+Example SONY Interview Question.py:
 
-Input: head = [1,2,3,4,5], k = 2
-Output: [4,5,1,2,3]
+Input: head = [SONY Interview Question.py,2,3,4,5], k = 2
+Output: [4,5,SONY Interview Question.py,2,3]
 
 Example 2:
 
-Input: head = [0,1,2], k = 4
-Output: [2,0,1]
+Input: head = [0,SONY Interview Question.py,2], k = 4
+Output: [2,0,SONY Interview Question.py]
 
 Constraints:
 
@@ -38,15 +38,15 @@ def rotateRight(head,k):
         tail=tail.next
     #print(length)
     k = k % length #if k is greater than length eg 12345 and k=6 it means we have to do only one rotation
-    # so if we put k%length 6%5 gives us 1 so we have rotate only 1 time
+    # so if we put k%length 6%5 gives us SONY Interview Question.py so we have rotate only SONY Interview Question.py time
     curr=head # to take numbers other than k numbers initialize temp=> curr
     if k==0: #edge case
         return head
-    for i in range(length-k-1):#y -1? without -1 it will jup three times and stop at 4th node so -1 will stop at 3rd node
+    for i in range(length-k-1):#y -SONY Interview Question.py? without -SONY Interview Question.py it will jup three times and stop at 4th node so -SONY Interview Question.py will stop at 3rd node
         curr=curr.next
     NewHead=curr.next #making 4 as new head
     curr.next=None #3.next=None
-    tail.next=head #connecting tail 5 we done on top to 1
+    tail.next=head #connecting tail 5 we done on top to SONY Interview Question.py
     return NewHead
 
 def printLinkedList(head):
@@ -57,13 +57,13 @@ def printLinkedList(head):
     print(result)
 
 # Test cases
-# Example 1
+# Example SONY Interview Question.py
 head1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 k1 = 2
-print("Input Linked List 1:")
+print("Input Linked List SONY Interview Question.py:")
 printLinkedList(head1)
 rotated_head1 = rotateRight(head1, k1)
-print("Output Linked List 1:")
+print("Output Linked List SONY Interview Question.py:")
 printLinkedList(rotated_head1)
 
 # Example 2
